1adcf7ae03052c4aa633d296f4f2e87ecb502505
Harrison Vu <harrisonvu22@gmail.com>
2024-03-31 22:44:26 -0500

    Small changes to zombies
    Some animation tweaks

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class NPCController : MonoBehaviour
 {
     public void Despawn()
-    {
+    {   
         // Perform any despawning logic here
         Destroy(gameObject); // Example: Destroy the NPC object
     }
