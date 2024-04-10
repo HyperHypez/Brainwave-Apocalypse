a7e012fd664c07eab37cd35127c781bb7f515a62
jakub23244 <jpeka2@uic.edu>
2024-04-09 22:07:35 -0500

    Title screen implemented

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    public CanvasGroup OptionPanel;
+
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Option()
+    {
+        OptionPanel.alpha = 1;
+        OptionPanel.blocksRaycasts = true;
+    }
+
+    public void Back()
+    {
+        OptionPanel.alpha = 0;
+        OptionPanel.blocksRaycasts = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
